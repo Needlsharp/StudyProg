4ba15842ff14312ad6ca4c64e12045d7263aff02
Needlsharp <dimasntu@gmail.com>
2023-03-03 15:49:55 +0300

    Add Task3

---
+++ Task3/Shortest Word/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Shortest_Word
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Assert.AreEqual(3, Kata.FindShort("bitcoin take over the world maybe who knows perhaps"));
+            Assert.AreEqual(3, Kata.FindShort("turns out random test cases are easier than writing out basic ones"));
+            Assert.AreEqual(2, Kata.FindShort("Let's travel abroad shall we"));
+        }
+    }
+
+    public class Kata
+    {
+        public static int FindShort(string s)
+        {
+            return s.Split(' ').Select(x => Convert.ToInt32(x.Length)).Min();
+        }
+    }
+}
+
