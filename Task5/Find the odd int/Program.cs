eb92440baf05386fe705c9e23dadcc5fdff0917d
Needlsharp <dimasntu@gmail.com>
2023-03-10 22:12:23 +0300

    Add Task5

---
+++ Task5/Find the odd int/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using System.Linq;
+
+namespace Find_the_odd_int
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assert.AreEqual(11, Kata.find_it(new[] {11}));
+        }
+    }
+
+    class Kata
+    {
+        public static int find_it(int[] seq)
+        {
+            int c = seq[0];
+            for (int i = 0; i < seq.Length; i++)
+            {
+                
+                c = seq[i];
+                if (seq.Count(x=> x == c) % 2 == 1) break;
+            }
+            return c;
+        }
+    }
+}
