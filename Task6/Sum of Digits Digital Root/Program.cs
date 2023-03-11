b6da76b7b50a97fb1f971eddb4fcc69cd6177365
Needlsharp <dimasntu@gmail.com>
2023-03-12 01:18:45 +0300

    Add Task 6

---
+++ Task6/Sum of Digits Digital Root/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Sum_of_Digits_Digital_Root
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            long n = 2;
+
+
+            int[] arr = Calculate(n);
+            
+            while (arr.Length > 1)
+            {
+                n = arr.Sum();
+                arr = Calculate(n);
+            }
+
+            Console.WriteLine(arr[0]);
+            int[] Calculate (long n) 
+            {
+                return n.ToString().Select(x => int.Parse(x.ToString())).ToArray();
+            }
+        
+
+            //foreach (var item in intList)
+            //{
+            //    Console.Write(item+" ");
+            //}
+            }
+    }
+
+}
